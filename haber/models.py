b8595ac160667024af915e0e38820693f41d9aa1
torealdemir <aldemirtore@gmail.com>
2023-10-30 19:48:21 +0300

    first commit

---
+++ haber/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Makale(models.Model):
+    yazar = models.CharField(max_length=150)
+    baslik = models.CharField(max_length=120)
+    aciklama = models.CharField(max_length=200)
+    metin = models.TextField()
+    sehir = models.CharField(max_length=120)
+    yayimlanma_tarihi = models.DateField()
+    aktif = models.BooleanField(default=True)
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True)
+    guncellenme_tarihi = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.baslik
